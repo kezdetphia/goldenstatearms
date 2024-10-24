d9767b90e96435748a60077b0fb01f6f9a5e330b
kezdetphia <fehermark88@gmail.com>
2024-10-23 18:22:17 -0700

    change name and favicon

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Golden State Arms",
+  description: "Gun Store in Azusa",
   navItems: [
     {
       label: "Home",
